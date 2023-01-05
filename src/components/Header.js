823f55b862acda62e0384535f54b6ec6fa030adb
Wantonfury <orzan.alexandru94@gmail.com>
2023-01-06 00:20:48 +0200

    Update Header to functional components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,13 @@
 import React from "react";
 import '../css/Header.css';
 
-class Header extends React.Component {
-    render() {
-        return (
-            <div id="header">
-                <h4>CV Maker</h4>
-                <span>Make and edit your CV!</span>
-            </div>
-        );
-    }
+const Header = () => {
+    return (
+        <div id="header">
+            <h4>CV Maker</h4>
+            <span>Make and edit your CV!</span>
+        </div>
+    );
 }
 
 export default Header;
